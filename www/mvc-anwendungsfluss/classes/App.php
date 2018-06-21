ff371e8e2666b12649e985f2efe16e574deffd84
Marten Stockenberg <MStockenberg@googlemail.com>
2018-06-21 12:26:19 +0200

    diagram push

---
+++ www/mvc-anwendungsfluss/classes/App.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mstockenberg
+ * Date: 21.06.18
+ * Time: 11:49
+ */
+
+namespace classes;
+
+
+class App
+{
+
+// erstelle init funktion
+    public function init()
+    {
+
+    }
+
+}
---
+++ www/mvc-anwendungsfluss/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mstockenberg
+ * Date: 21.06.18
+ * Time: 11:24
+ */
+
+require_once 'vendor/autoload.php';
+
+$app = new \classes\App();
+$app->init();
